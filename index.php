9bf06cef8c9078f3c7591bc593811ccdf854a34f
Saifulla <Saifulla1228@gmail.com>
2020-09-01 21:06:27 +0500

    inital commit

---
+++ index.php
@@ -0,0 +1,217 @@
+<!doctype html>
+<html lang="en">
+  <head>
+ 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    
+  </head>
+  <body>
+ 
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="5.png" class="d-block w-100 p-3" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="6.png" class="d-block w-100 p-3" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-bright py-2 pl-5 ">
+  <a class="navbar-brand" href="#">Ihavandhoo School</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Profile</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Print Room</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Text Book</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Lesson plan</a>
+      </li>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Timetable</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Attendance</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="class mb-3 p-4"> 
+<div class="row" style="background-color:#74b6c6;">
+<div class="col-12 py-2" style="background-color:#74b6c6">
+
+<h1> About School</h1>
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">School</h5>
+        <p class="card-text">Opened on 14 May 1979, Ihavandhoo School serves as the public school in the island, providing primary to higher secondary education. ... The school became a government owned school in 2005. Today, this school has almost the largest student population in the entire atoll.</p>
+        <img src="logo.png" width=30% alt="">
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">School Mission</h5>
+        <p class="card-text">Our vision is to empower students to acquire, demonstrate, articulate and value knowledge and skills that will support them, as life-long learners, to participate in and contribute to the global world and practise the core values of the school: respect, tolerance & inclusion, and excellence</p>
+        <img src="logo1.png" width=22% alt="">
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+</div>
+<h1 id="education" class="text-center">Up coming events</h1>
+<div class="class mb-3 p-1"> 
+<div class="row" style="background-color:#74b6c6;">
+<div class="col-12 py-2" style="background-color:#74b6c6">
+<div class="row">
+
+
+            <div class="col-md-12 offset-md-12 mb-3 p-2">
+                <div class="card d-flex flex-md-row shadow">
+                
+                <br>
+
+                <p class="card-text">  Sport Meet <br>Do you remember your School Sports Day? It was a great excuse to get out in the sunshine and have loads of fun, running around and enjoying yourself - well, make sure you’ve packed your kit as Sunshine Events take you back to your school days with this great package of classic races (or more extreme races).  We’ve got everything you need to create a fun Sports Day for your colleagues, family and friends. 
+
+Relive your youth with some sporty activities! Sunshine Events can supply the equipment and all you need to do is organise your teams and get them moving.  But watch out for the cheaters!
+
+<br>
+Egg & Spoon Race
+Sack Racing
+Walk The Plank
+Space Hoppers
+Ski Slalom</p>
+                    <div class="bg-white  align-self-center my-auto">
+                    
+                        <img src="logo.png" width="200px">
+                    </div>
+                    <div class="p-3 my-auto">
+                    
+    </div>
+    <div class="card-columns">
+  <div class="card">
+    <img src="meeting.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Staff Meeting</h5>
+      <p class="card-text">30 August 2020</p>
+    </div>
+  </div>
+  <div class="card p-3" style="background-color:#90EE90;">
+    <blockquote class="blockquote mb-0 card-body">
+      <p>Scout Camp.</p>
+      <footer class="blockquote-footer">
+        <small class="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card" style="background-color:#74b6c6;">
+    <img src="prizeday.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">School Prize Day</h5>
+      <p class="card-text">21 October 2020.</p>
+      <p class="card-text"><small class="text-muted"></small></p>
+    </div>
+  </div>
+  <div class="card bg-primary text-white text-center p-3">
+    <blockquote class="blockquote mb-0">
+      <p>Key 1 and 2</p>
+      <footer class="blockquote-footer text-white">
+        <small>
+         Cordination <cite title="Source Title">Every THursday</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card text-center" style="background-color:#74b6c9;">
+    <div class="card-body">
+      <h5 class="card-title">English Day 2020</h5>
+      <p class="card-text">11 September 2020</p>
+      <p class="card-text"><small class="text-muted"></small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="tea.jpg" class="card-img-top" alt="...">
+  </div>
+  <div class="card p-3 text-right" style="background-color:#97b6c6;">
+    <blockquote class="blockquote mb-0">
+      <p>Teachers' Day</p>
+      <footer class="blockquote-footer">
+        <small class="text-muted">
+          October 5 <cite title="Source Title">2020</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card" style="background-color:yellow;>
+    <div class="card-body">
+      <h5 class="card-title">Dhivehi Day 2020</h5>
+      <p class="card-text"> 3 November 2020.</p>
+      <p class="card-text"><small class="text-muted"></small></p>
+    </div>
+  </div>
+</div>
+    
+    </div>
+  </div>
+</div>
+<div class="col-lg-12  p-5" style= background-color:grey;>
+                <div>
+                    <address mt-5>Sample Page,<br>Saifulla Ahmed</address>
+                    
+                </div>   
+</div>
+</div>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
