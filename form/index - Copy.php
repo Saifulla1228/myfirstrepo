36d1706471d789c911524fa6e5190e6d0660bb42
Saifulla <Saifulla1228@gmail.com>
2020-09-01 21:13:41 +0500

    all other file

---
+++ form/index - Copy.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<style>
+
+   
+}
+</style>
+
+  </head>
+
+  <body>
+  
+<div class="box1 color:red">
+<h1>Box1</h1>
+</div>
+
+
+
+
+
+  <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item active mt-3" aria-current="page">Saifulla</li>
+  </ol>
+</nav>
+
+
+</div>
+  <div class="mainContainer">
+  <div class="text-center">
+  <div class="col-md-4 offset-md-4">
+  <h2>My first form </h2>
+  <h5>Saifulla Ahmed</h5>
+  </div>
+  
+
+  
+  </div>
+
+  <div class="container">
+  <div class="col-md-4 offset-md-4 pt-5">
+  <div class="card" style="width: 34rem;">
+  <div class="card-body">
+  <form>
+  <div class="form-group row">
+    <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control form-control-sm" id="Email" Email="col-form-label-sm">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Subject</label>
+    <div class="col-sm-10">
+      <input type="Subject" class="form-control form-control-sm" id="Subject" Subject="col-form-label-sm">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Message</label>
+    <div class="col-sm-10">
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="6"></textarea> <br>
+  <button type="Send">Send</button>
+  </div>
+
+  </div>
+  </div>
+  
+</form>
+  </div>
+</div>
+  
+
+
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    </div>
+    </div>
+  </body>
+  
+</html>
